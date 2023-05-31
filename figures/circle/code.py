9e1331c69b5672ff2875cf31d5333537d3fddb5e
old <sobirjon>
2023-05-31 07:03:35 +0500

    circle

---
+++ figures/circle/code.py
@@ -0,0 +1,9 @@
+def circle_perimeter(default_radius):
+    perimeter = (2 * 3.14 * default_radius)
+    print(perimeter)
+
+
+def circle_area(default_radius):
+    area = 3.14 * (default_radius ** 2)
+    print(area)
+
