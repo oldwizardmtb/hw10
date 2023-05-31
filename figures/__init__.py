4d2bd83988b2531f2d3b96f42b45c8af80fc69e0
old <sobirjon>
2023-05-31 07:12:39 +0500

    init file

---
+++ figures/__init__.py
@@ -0,0 +1,10 @@
+from circle.code import circle_perimeter, circle_area
+from square.code import square_area, square_perimeter
+from triangle.code import triangle_area, triangle_perimeter
+
+circle_perimeter(default_radius=5)
+circle_area(default_radius=5)
+square_perimeter(default_a=15)
+square_area(default_a=15)
+triangle_perimeter(a=7, b=2, c=8)
+triangle_area(a=7, b=2, c=8)
