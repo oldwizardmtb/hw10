2424ad27d094d1207f395c7173567ceb1ba8d5fa
old <sobirjon>
2023-05-31 07:16:03 +0500

    all

---
+++ main.py
@@ -0,0 +1,18 @@
+# a = int(input('Enter num: '))
+# print(sum(map(int, list(str(a)))))
+
+# sec = int(input('your seconds: '))
+# a = (sec // 86400)
+# aS = sec % 86400
+# print(a)
+# b = aS // 3600
+# bS = aS % 3600
+# print(b)
+# c = bS // 60
+# print(c)
+# d = bS % 60
+# print(d)
+# print('In', sec, 'seconds - ', a, 'days', b, 'hours', c, 'minutes', d, 'seconds')
+
+
+
